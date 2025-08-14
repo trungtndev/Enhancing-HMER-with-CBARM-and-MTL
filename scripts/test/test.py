90d0a77242f90d72167a3ab591be9eb6a9bb829f
trung <trunglh113@gmail.com>
2025-08-14 10:21:01 +0700

    add new data

--- scripts/test/test.py
+++ scripts/test/test.py
@@ -1,8 +1,6 @@
 import os
 import sys
 
-# ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-# sys.path.insert(0, ROOT_DIR)
 import typer
 from mtl.datamodule import CROHMEDatamodule
 from mtl.lit_mtl import LitMTL
